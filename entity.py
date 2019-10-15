0501a131415abe2d99aa84f65d61c93c45ad336d
CGG9517 <840065231@qq.com>
2019-10-15 20:36:39 +0800

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,5 +1,19 @@
-
-class Hanzi:
+class Ciyu:
     """
     汉字基本类
     """
+
+    def __init__(self, _quanpin, _hanzi, _shouxie):
+        self.quanpin = _quanpin
+        self.hanzi = _hanzi
+        self.shouxie = _shouxie
+
+
+class Phrase:
+    def __init__(self, _line):
+        self.line = _line
+
+
+if __name__ == '__main__':
+    new_ciyu = Ciyu("zhongguo", "中国", "zg")
+    print(new_ciyu.quanpin)
